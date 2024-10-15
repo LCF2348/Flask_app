00b31e9002bd40c0456675729b6d9654bacf65ad
LCF2348 <153139671+LCF2348@users.noreply.github.com>
2024-10-15 13:46:31 -0400

    Updated Flask app with separate information in .env

--- app.py
+++ app.py
@@ -7,8 +7,16 @@ load_dotenv()
 
 app = Flask(__name__)
 
-#connection
-CONNECTION_STRING = os.getenv('MONGO_URI')
+#get env stuff
+MONGODB_USERNAME = os.getenv('MONGODB_USERNAME')
+MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
+MONGO_HOST = os.getenv('MONGO_HOST')
+MONGO_DB = os.getenv('MONGO_DB')
+
+# Construct the MongoDB URI
+CONNECTION_STRING = f"mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@{MONGO_HOST}/{MONGO_DB}?retryWrites=true&w=majority&appName=Cluster0"
+
+
 
 client = MongoClient(CONNECTION_STRING)
 db = client.shop_db 
